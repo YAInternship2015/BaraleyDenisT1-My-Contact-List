1de111e6f4f3c1f12e1de67cf9fdffd654018c93
mcat345 <mcat345@gmail.com>
2015-08-10 14:22:26 +0300

    Initial Commit

---
+++ My Contact List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Contact List
+//
+//  Created by Admin on 10.08.15.
+//  Copyright (c) 2015 BaraleyDenis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ My Contact List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Contact List
+//
+//  Created by Admin on 10.08.15.
+//  Copyright (c) 2015 BaraleyDenis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
